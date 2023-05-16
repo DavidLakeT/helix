21742f2c066e3c00bbe943c035e3d25600b01163
jdvalencit <jdvalencit@eafit.edu.co>
2023-05-16 18:58:31 -0500

    Instance creation and autoscaling group attachment

--- controller/controller.py
+++ controller/controller.py
@@ -1,12 +1,10 @@
 import boto3
 
-# Especificar las credenciales de AWS
-access_key = 'ASIA2JWTJAFB52TTTLET'
-secret_key = 'mBXXGl+ZFy8Av2CJCBklasBbM8tOhESmY6syOWtV'
-session_token = 'FwoGZXIvYXdzEBwaDONJHmCRDpN1RDcEASLIAXW1toY4ot3GD/R5+mDist67NhqJ/P/hR8OqYPKz0m5jA/Fuvy+HNdOkud2Vci0JmsFzHyWgKuHe40jefVzgSdCkDeQ9IJqa3a01TASm6DDAmgaAP3f/X9v4xKOxuSbobMMkaaAxTLYhOwtfM2/5imKjG6UBTwIM2l1VOXpseXihVR4TC5lcyr9h/lcvDNfv3yxy98s+rKnajoH6LvPYIb0m2DPH+k6aL3JP/dn9ZgG70vB/2QZXCzMEhAl8/117cXp6LebHVJxZKMyQj6MGMi2I9S1T4XYSZnoVXfFePy4EuScDvjUd2BNAWRYHqVMfNswcIVqCX8BbBWkeahA='
+access_key = 'ASIA2JWTJAFBYHHO6PPF'
+secret_key = 'zco+T1lLfWPJKcCAhwAlF9vhTZmg0U4sVE+BXLUA'
+session_token = 'FwoGZXIvYXdzECAaDLq9LyNQPUxobabgOyLIAYSMsTShG+JRZKKp3M+VkQxEaQKZ1UzCOYHUPzGSJQe23WSeCks1QJlsKP8TouzefZr3kGmWDW/HPWe0zRyvMfEA4hfPU1HRFnAMnMzb9mn5J/MR6xVwYDkaxlxJRjf09FjNEpsVNTWdRlTElmKd3WF09CWrkshfLX/p4jkhdPKAf5PnIKq4tPc/I2DQkM/NLSC214KStp1ENlSS01xbPrdYa1x8m4rC39EkwG7dQJjAnQpWjr+emPmrWkUIpYoWFzmQG2TiHZBgKMCFkKMGMi2JsSDiNIL5V/QRyv9UZLnkQ3CTR5haOa4///8Y29rRLItI0KTb6Sazj3PBtKg='
 region = 'us-east-1'
 
-# Crear una instancia del cliente EC2 con las credenciales especificadas
 client = boto3.client(
     'ec2',
     aws_access_key_id=access_key,
@@ -14,42 +12,55 @@ client = boto3.client(
     aws_session_token=session_token,
     region_name=region,
 )
-# Resto del código...
-
-# Obtener todas las instancias EC2
-response = client.describe_instances()
-
-# Iterar sobre las reservas
-for reservation in response['Reservations']:
-    # Iterar sobre las instancias dentro de cada reserva
-    for instance in reservation['Instances']:
-        # Comprobar si la instancia tiene un nombre
-        if 'Tags' in instance:
-            for tag in instance['Tags']:
-                if tag['Key'] == 'Name':
-                    # Imprimir el nombre de la instancia
-                    print("Nombre de la instancia:", tag['Value'])
-        else:
-            print("Nombre de la instancia: (Sin nombre)")
 
 
-'''instance = client.create_instances(
-    ImageId='AMI_ID',  # Reemplaza con el ID de la imagen AMI que desees
+def list_instances():
+    response = client.describe_instances()
+
+    for reservation in response['Reservations']:
+        for instance in reservation['Instances']:
+            if 'Tags' in instance:
+                for tag in instance['Tags']:
+                    if tag['Key'] == 'Name':
+                        print("Nombre de la instancia:", tag['Value'])
+            else:
+                print("Nombre de la instancia: (Sin nombre)")
+
+
+list_instances()
+
+instance = client.run_instances(
     MinCount=1,
     MaxCount=1,
-    InstanceType='t2.micro',  # Reemplaza con el tipo de instancia que desees
-    KeyName='NOMBRE_PAR_LLAVES',  # Reemplaza con el nombre de tus llaves existentes
-    SecurityGroupIds=['ID_DEL_GRUPO_DE_SEGURIDAD'],  # Reemplaza con el ID del grupo de seguridad que desees
+    ImageId='ami-0557a15b87f6559cf',
+    InstanceType='t2.micro',
+    KeyName='vockey',
+    SecurityGroupIds=['default'],
     UserData='''#!/bin/bash
                 # Incluye cualquier configuración adicional que desees ejecutar en la instancia al inicio
-                echo "Configuración adicional"''',  # Reemplaza con los comandos de configuración adicionales que desees ejecutar
+                echo "Configuración adicional"''',
     TagSpecifications=[{
         'ResourceType': 'instance',
-        'Tags': [{'Key': 'Name', 'Value': 'NOMBRE_DE_INSTANCIA'}]  # Reemplaza con el nombre que desees asignar a la instancia
+        'Tags': [{'Key': 'Name', 'Value': 'test_instanceC'}]
     }]
 )
+print("\n")
+instance_id = instance['Instances'][0]['InstanceId']
+list_instances()
 
-print("Se creó la instancia con ID:", instance[0].id)'''
+client.get_waiter('instance_running').wait(InstanceIds=[instance_id])
 
+autoscaling_client = boto3.client(
+    'autoscaling',
+    aws_access_key_id=access_key,
+    aws_secret_access_key=secret_key,
+    aws_session_token=session_token,
+    region_name=region,
+)
 
+group_name = 'Proyecto2 ScalingGroup'
 
+response = autoscaling_client.attach_instances(
+    InstanceIds=[instance_id],
+    AutoScalingGroupName=group_name
+    )
