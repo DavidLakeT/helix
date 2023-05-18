57c759e781d63d13f77707e2ebd33c395597f02a
jdvalencit <jdvalencit@eafit.edu.co>
2023-05-18 00:59:42 -0500

    Deleted autoscaling group implementation

--- controller/controller.py
+++ controller/controller.py
@@ -1,19 +1,18 @@
 import boto3
 import random
 import time
-from instance_manager import new_instance, delete_instance, cleaner, instance_setup
+from instance_manager import create_instance, terminate_instance, cleaner, instance_setup
 
 access_key = 'ASIA2JWTJAFB57L2S7AZ'
 secret_key = '+aSPnyTspSwYZ1sRqb3pnAJWVXP7xMM1UBUYiViU'
 session_token = 'FwoGZXIvYXdzEDgaDHI/yCMEjOvLGDIWeyLIAaIUUCs/5WAXzZvBf7xZQYQ6hDakNSNbOQPjP1qLgk1q0tStbdR/SSxvKbwC7PIqg8nbICrGHaC3MploPwXeC2ZH0TWfMm8XHFzW/m1wFFff8obKa/KpAh13TBE8n1hVybhRmcXwq22UH62kbRZE5IH7z9pDtu36nE2kxNmBLhTsJlnbJtavjUsD0adNvFCaF4CMUO5Rt6sAuc7AirPsU/F14z+D7XwYROJOl61InpRfan2vaBRiLsTOK0gkQmgIix+O1FDN6zj1KKu2laMGMi2/FBoPS6VVyg/G46/6h7AAPZ+TqBf3IuY224PcbghBrnsJSS0l7oSidXtzQYo='
 region = 'us-east-1'
 
-group_name = 'Proyecto2 ScalingGroup'
 instance_id = 'i-0c815d2e545a64860'
-cpu_upper_limit = 0.7
 cpu_lower_limit = 0.3
-max_instances = 5
+cpu_upper_limit = 0.7
 min_instances = 2
+max_instances = 5
 instance_list = []
 
 ec2_client = boto3.client(
@@ -24,20 +23,11 @@ ec2_client = boto3.client(
     region_name=region,
 )
 
-autoscaling_client = boto3.client(
-    'autoscaling',
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key,
-    aws_session_token=session_token,
-    region_name=region,
-)
-
-cleaner(ec2_client, autoscaling_client, group_name)
-id1, id2 = instance_setup(ec2_client, autoscaling_client, group_name)
+cleaner(ec2_client)
+id1, id2 = instance_setup(ec2_client)
 instance_list.append(id1)
 instance_list.append(id2)
 current_instances = 2
-
 
 while True:
     cpu_avg = random.random()
@@ -45,7 +35,7 @@ while True:
     if cpu_avg > cpu_upper_limit and current_instances < max_instances:
 
         print("Creamos nueva instancia\n")
-        instance, _ = new_instance(ec2_client, autoscaling_client, "", group_name)
+        instance = create_instance(ec2_client)
         instance_list.append(instance)
         current_instances += 1
         print("Instancias activas: " + str(current_instances) + "\n")
@@ -54,9 +44,9 @@ while True:
 
         print("Eliminamos una instancia\n")
         instance_to_delete = instance_list.pop(0)
-        delete_instance(ec2_client, autoscaling_client, instance_to_delete, group_name)
+        terminate_instance(ec2_client, instance_to_delete)
         current_instances -= 1
         print("Instancias activas: " + str(current_instances) + "\n")
 
     print("-------------------------------------------------")
-    time.sleep(10)
+    time.sleep(15)
--- controller/instance_manager.py
+++ controller/instance_manager.py
@@ -5,62 +5,36 @@ def list_instances(client, group_name):
     return response['AutoScalingGroups'][0]['Instances']
 
 
-def create_instance(client, name):
-    return client.run_instances(
+def create_instance(client):
+    instance = client.run_instances(
         MinCount=1,
         MaxCount=1,
-        ImageId='ami-0557a15b87f6559cf',
+        ImageId='ami-04f27063c8ae01651',
         InstanceType='t2.micro',
         KeyName='vockey',
         SecurityGroupIds=['default'],
         UserData='',
         TagSpecifications=[{
             'ResourceType': 'instance',
-            'Tags': [{'Key': 'Name', 'Value': name}]
-        }]
-    )
+            'Tags': [{'Key': 'Name', 'Value': ''}]
+        }])
+    instance_id = instance['Instances'][0]['InstanceId']
+    client.get_waiter('instance_running').wait(InstanceIds=[instance_id])
+    return instance_id
 
 
 def terminate_instance(client, instance_id):
     return client.terminate_instances(InstanceIds=[instance_id])
 
 
-def attach_instance(client, instance_id, group_name):
-    return client.attach_instances(
-        InstanceIds=[instance_id],
-        AutoScalingGroupName=group_name
-        )
+def cleaner(ec2_client):
+    description = ec2_client.describe_instances()
+    for reservation in description['Reservations']:
+        for instance in reservation['Instances']:
+            terminate_instance(ec2_client, instance['InstanceId'])
 
 
-def detach_instance(client, instance_id, group_name):
-    return client.detach_instances(
-        InstanceIds=[instance_id],
-        AutoScalingGroupName=group_name,
-        ShouldDecrementDesiredCapacity=True
-        )
-
-
-def new_instance(ec2_client, autoscaling_client, name, group_name):
-    instance = create_instance(ec2_client, name)
-    instance_id = instance['Instances'][0]['InstanceId']
-    ec2_client.get_waiter('instance_running').wait(InstanceIds=[instance_id])
-    at_response = attach_instance(autoscaling_client, instance_id, group_name)
-    return (instance_id, at_response)
-
-
-def delete_instance(ec2_client, autoscaling_client, instance_id, group_name):
-    dt_response = detach_instance(autoscaling_client, instance_id, group_name)
-    terminate = terminate_instance(ec2_client, instance_id)
-    return (dt_response, terminate)
-
-
-def cleaner(ec2_client, autoscaling_client, group_name):
-    instances = list_instances(autoscaling_client, group_name)
-    for instance in instances:
-        delete_instance(ec2_client, autoscaling_client, instance['InstanceId'], group_name)
-
-
-def instance_setup(ec2_client, autoscaling_client, group_name):
-    id1, _ = new_instance(ec2_client, autoscaling_client, '', group_name)
-    id2, _ = new_instance(ec2_client, autoscaling_client, '', group_name)
+def instance_setup(ec2_client):
+    id1 = create_instance(ec2_client)
+    id2 = create_instance(ec2_client)
     return (id1, id2)
